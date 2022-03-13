a7039f2d1476dbd5a74661d022e5eb9d99a84ddd
651544537@qq.com <651544537@qq.com>
2022-03-13 16:27:50 +0800

    I have down some test on this file.

--- Bubble/bubble_sort.cpp
+++ Bubble/bubble_sort.cpp
@@ -1,13 +1,14 @@
 #include "../Founction.H"
 
 int main() {
+	init();
 	rectangle data;
 	int length = 0;
 	ParameterInit(&length); 
 	InitRectangle(data, length);
 	CreatRectangle(data);
 	Visualization(data);
-	DrawAColumn(0, 35);
+	DrawAColumn(0, 35, FOREGROUND_RED);
 	gotoxy(0, RANGE + 10);
 	system("pause");
 	return 0;
